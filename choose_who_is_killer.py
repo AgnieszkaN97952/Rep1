53b442ad759e05744bbae4fba1ae9ded2d6f03a0
Szymon Zenka <85746@student.ue.poznan.pl>
2025-03-10 10:38:40 +0100

    Adding code in file choose_who_is_killer.py

---
+++ choose_who_is_killer.py
@@ -0,0 +1,9 @@
+print("Pseudonim mordercy to Blackie, a jego prawdziwa tożsamość to...")
+odpowiedz_uzytkownik = input("/n a) Jan Grabarz 1 /n b) Gustaw Czarnecki /n c) Cezary Kielecki /n Jako odpowiedz wpisz np.: a")
+def rozpoznaj_morderce(odpowiedz_uzytkownik):
+    if(odpowiedz_uzytkownik)=="b":
+        print("Poprawna odpowiedz!")
+    else:
+        print("Bledna odpowiedz!")
+
+rozpoznaj_morderce(odpowiedz_uzytkownik)
